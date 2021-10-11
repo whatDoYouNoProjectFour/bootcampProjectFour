f4a044ee9221521c935653e2c6569d5add0b08fc
CalAkler <calvinakler@gmail.com>
2021-10-10 22:34:38 -0400

    commented and cleaned up code

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
   // hardcoded array of 10 homophonous words
   const randomWords = ['air', 'coarse', 'knot', 'principal', 'flour', 'idle', 'stationary', 'maid', 'prophet', 'their'];
 
-  // const [randomWord, setRandomWord] = useState('')
-  const [definition, setDefinition] = useState('')
+  // declare state variables 
+  const [definition, setDefinition] = useState('');
   const [combinedWords, setCombinedWords] = useState([]);
 
   // function to randomly select an item from an array
@@ -22,54 +22,71 @@ function App() {
   useEffect(() => {
     // get random word from hardcoded array to pass into axios query param
     const randomNum = randomize(randomWords);
-    const currentWord = randomWords[randomNum];
+    const startingWord = randomWords[randomNum];
     axios({
       url: 'https://api.datamuse.com/words',
       method: 'GET',
       dataResponse: 'json',
       params: {
         md: "d",
-        rel_hom: currentWord,
+        rel_hom: startingWord,
       }
-    }).then(res => {
-      // filter returned words for words that have valid definitions
-      const wordWithDefinition = res.data.filter(res => res.defs);
-
+    }).then(homophone => {
+      // filter returned words for words that have valid definitions and store in state
+      const wordWithDefinition = homophone.data.filter(homophone => homophone.defs);
       setDefinition(wordWithDefinition[0].defs[0]);
-      let unshuffled = [wordWithDefinition[0].word + `(data from api)`, currentWord]
-      // added sort property to use sort array built-in function.
-      let shuffled = unshuffled.map(val => {
-        if (wordWithDefinition[0].word === val) {
+
+      const unshuffled = [wordWithDefinition[0].word, startingWord]
+      // map unshuffled array to produce a new array of shuffled objects that contain the word and definition
+      const shuffled = unshuffled.map(word => {
+        // condition to check if word has a definition (always the api result) and return an object with that property
+        if (wordWithDefinition[0].word === word) {
           return ({
-            val,
+            word,
             definition: wordWithDefinition[0].defs[0],
             sort: Math.random(),
           })
+          // don't return definition property if it is the hardcoded original word
         } else {
           return ({
-            val,
+            word,
             sort: Math.random(),
           })
         }
       })
-        // shuffled by sort value
+        // use sort method to randomly change order of objects in array
         .sort((a, b) => a.sort - b.sort)
 
-      setCombinedWords(...combinedWords, shuffled);
+      // store shuffled result in state
+      setCombinedWords(shuffled);
+      console.log(shuffled);
     })
+    // needs to be dependant on click event handler -- ADD IN ONCE FUNCTION IS FINISHED
   }, []);
 
   console.log(combinedWords);
 
+  // event handler to evaluate if word matches definition and increases score
+  const handleClick = (e, index) => {
+    console.log(e.target.textContent);
+
+    // FIX ME
+    if (e.target.textContent === combinedWords[1].word && combinedWords[1].hasOwnProperty(definition)) {
+      console.log('you got it!');
+    } else {
+      console.log('wrong :(');
+    }
+  }
+
+
   return (
     <div className="App">
-
       <h1>What Do You No?</h1>
       {
-        combinedWords.map((res, index) => {
+        combinedWords.map((individualWord, index) => {
           return (
-            <button key={index}>
-              {res.val}
+            <button key={index} onClick={handleClick}>
+              {individualWord.word}
             </button>
           )
         })
