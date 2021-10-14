69fdc79fecf3dfbe11c135ee63bf4eb42a2d7d8b
Jun Choi <cjh4549@gmail.com>
2021-10-13 20:17:56 -0400

    stashing minor changes in files

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -2,7 +2,7 @@ import { Line } from 'rc-progress';
 
 const ProgressBar = ({progress}) => (
   <>
-     <Line percent={progress} strokeWidth="1" trailWidth="5" strokeColor="#6056f9" strokeLinecap='square'/>
+     <Line percent={progress} strokeWidth="1" trailWidth="1" strokeColor="#6056f9" strokeLinecap='square'/>
   </>
 );
 
