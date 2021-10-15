694664449782fc75b27d9f406d2ecbb75f30bb5a
CalAkler <calvinakler@gmail.com>
2021-10-14 23:18:32 -0400

    homepage styles

--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 const PlayGame = () => {
   return (
     <>
-      <h2 className="play">Click to start!</h2>
-      <Link to={"game"} className="startButton">Start!</Link>
-      <p className="welcome">Welcome to What Do You No! Match the correct homophone with the definition to score points. Submit your score to see how you stack up. Have fun!</p>
-      <p> - Cal, Jun, Sara, and Seungmin</p>
+      <h2 className="play">Welcome to What Do You No?</h2>
+      
+      <p className="welcome"> Match the correct homophone with the definition to score points. Submit your score to see how you stack up. Have fun!</p>
+      <p className="signature"> - Cal, Jun, Sara, and Seungmin</p>
+      <Link to={"game"} className="startButton">Click to Start!</Link>
     </>
   )
 }
