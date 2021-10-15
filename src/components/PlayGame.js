71a5f9c7135337fc850859c29068cb8daeb0b69d
CalAkler <calvinakler@gmail.com>
2021-10-14 22:59:02 -0400

    button fix

--- src/components/PlayGame.js
+++ src/components/PlayGame.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 const PlayGame = () => {
   return (
     <>
-      <h2 className="playButton">Click to start!</h2>
-      <Link to={"game"}>Start!</Link>
+      <h2 className="play">Click to start!</h2>
+      <Link to={"game"} className="startButton">Start!</Link>
       <p className="welcome">Welcome to What Do You No! Match the correct homophone with the definition to score points. Submit your score to see how you stack up. Have fun!</p>
       <p> - Cal, Jun, Sara, and Seungmin</p>
     </>
